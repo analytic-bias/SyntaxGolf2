5faa3349ad8d46417681eafce1ffd360990220b7
Hypatia du Bois-Marie <knight.of.lambda.calculus@gmail.com>
2024-06-14 20:22:43 -0400

    reset

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ let refnodes = refskeleton.map((x) => ({
     linktarget: x.data.target,
   },
 }));
+refnodes[0].data.root = true;
 let refedges = refskeleton.reduce((ys, x) => x.parent ? ys.concat([{
     data: { id: crypto.randomUUID(), source: x.parent.id, target: x.id },
   }]) : ys, [])
@@ -126,6 +127,7 @@ refwords.forEach((x, i) => {
 }, [])
 // ENDFIX
 
+// let cy;
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("problemexp").innerHTML = testast;
   document.getElementById("problemast").innerHTML = sast;
@@ -199,6 +201,27 @@ document.addEventListener("DOMContentLoaded", function () {
       },
     ],
   });
+  let linktargets = cy.$('[linktarget]')
+  cy.$('[linksource]').forEach((x) => {
+    let t = linktargets.find(y => y.data('linktarget') === x.data('linksource'))
+    const id = crypto.randomUUID()
+    cy.add({ group: 'edges',
+      data: { 
+        virtual: true,
+        id: id,
+        source: t.incomers()[0].source().id(),
+        target: x.incomers()[0].source().id(),
+      },
+    })
+    x.data('virtualedge', id)
+    t.data('virtualedge', id)
+  })
+  cy.$('edge[virtual]').style({
+    'line-style': 'dashed',
+    'curve-style': 'unbundled-bezier', // FIXME bezier
+    'control-point-distances': '-200',
+    'control-point-weights': '0.5',
+  })
 
   cy.on('dragpan', function (evt) {
     const e = cy.extent();
@@ -211,18 +234,58 @@ document.addEventListener("DOMContentLoaded", function () {
     if (e.y2 - 2 * e.h > 0)
       cy.panBy({ y: e.y2 - 2 * e.h, x: 0 });
   });
+  // cy.$(':parent').forEach((x) => x.unselectify());
+  cy.on('select', function (evt) {
+    if (evt.target.isParent() || evt.target.isEdge())
+      evt.target.unselect()
+  })
+  // const wordsbelow = (x) => x.outgoers().children().filter((y) => y.data('content'))
+  cy.on('cxttap', function (evt) {
+    let outgoers = cy.$(':selected')[0] // FIXME [0] not very robust
+      .parent().outgoers()
+    let selectedvirtual = cy.$(':selected').findIndex(x => x.data('virtualtarget'))
+    let potentials = outgoers.map(x => 
+      x.target().children()
+       .filter(y => selectedvirtual > -1 || !y.data().virtualtarget)
+       .map(y => y.data().content)
+    )
+    let selection = cy.$(':selected').map(x => x.data().content)
+    let hit = potentials.findIndex(x => JSON.stringify(x) === JSON.stringify(selection))
+    if (hit > -1) {
+      outgoers[hit].style('visibility', 'visible')
+      outgoers[hit].target().style('visibility', 'visible')
+      outgoers[hit].target().children().forEach(x => {
+        if (x.data('virtualtarget')) {
+          console.log(cy.$('edge[virtual]').map(z => z.data()))
+          console.log(x.parent().id())
+          console.log(x.id())
+          cy.$('edge[virtual]').filter(y => y.data('source') === x.parent().id()).style('visibility', 'visible')
+          // console.log(x.parent().outgoers())
+          // x.parent().outgoers().filter(y => y.data('virtual')).forEach(y => y.style('visibility', 'visible'))
+        }
+      });
+    } else {
+    }
+    cy.$(':selected').unselect()
+  });
   // cy.snapToGrid();
   // cy.snapToGrid("snapOn");
   // cy.snapToGrid("gridOn");
 
-  interactivity();
+  // reset(cy);
 });
 
 // ------------------------------
-function interactivity() {
+function reset(cy) {
   cy.elements = {
     nodes: refnodes.concat(refsubnodes),
     edges: refedges,
   }
-  // TODO start and restart interactivity
+  let hidenodes = cy.$(':parent[^root], :orphan[^root]')
+  let hideedges = hidenodes.connectedEdges()
+  hidenodes.style('visibility', 'hidden')
+  hideedges.forEach(x => x.style('visibility', 'hidden'))
+
+  // TODO drag movement
+  // TODO choose tag
 }
